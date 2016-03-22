f769207c9024206c0e59adf6febfadcf9577e3d1
AdamJackman <adam.jackman@mail.utoronto.ca>
2016-03-22 00:35:55 -0400

    tether base has been added

---
+++ public/src/js/tour.js
@@ -0,0 +1,23 @@
+//Let's tour my site
+var tour = new Shepherd.Tour({
+		defaults: {
+		classes: 'shepherd-theme-arrows',
+		scrollTo: true
+		}
+});
+//Tour Steps
+tour.addStep('example-step', {
+  text: 'This step is attached to the bottom of the <code>.example-css-selector</code> element.',
+  attachTo: '#main bottom',
+  classes: 'example-step-extra-class',
+  buttons: [
+    {
+      text: 'Next',
+      action: tour.next
+    }
+  ]
+});
+//Start the tour
+$(document).ready(function(){
+	//tour.start();
+});
